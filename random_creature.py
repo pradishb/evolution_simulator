ad91832d7db76549621bdb824e187c822d2cd553
Biraj Rajbhandari <birajr0@gmail.com>
2019-07-16 18:55:56 +0545

    Adds new algorithm for random creature

--- random_creature.py
+++ random_creature.py
@@ -1,19 +1,29 @@
-from fisher_yates_shuffle import shuffle
-from random import randint
+"Module to generate random connected graphs"
+from random import choice, randint
+
 import cv2
 import numpy as np
 
 
-def create_species(n):
-    shuffled = shuffle(list(range(n)))
-    print(shuffled)
-    edges = []
-    for i in range(n - 1):
-        edges.append((shuffled[i], shuffled[i+1]))
-    return edges
+def create_edges(size):
+    "Generation of random graph"
+    all_edges = set()
+    for vertex_a in range(size):
+        for vertex_b in range(vertex_a+1, size):
+            all_edges.add((vertex_a, vertex_b))
+    edges = set(
+        filter(lambda edge: edge[0] + 1 == edge[1], all_edges))
+
+    remaining_edges = all_edges - edges
+    for _ in range(randint(0, len(remaining_edges))):
+        random_edge = choice(list(remaining_edges))
+        remaining_edges -= {(random_edge)}
+        edges = edges.union({(random_edge)})
+    return list(edges)
 
 
 def create_vertices(n):
+    "Generation of random vertices"
     vertices = []
     for _ in range(n):
         vertices.append((randint(0, 500), randint(0, 500)))
@@ -21,6 +31,7 @@ def create_vertices(n):
 
 
 def draw_creature(vertices, edges):
+    "Draws connected graph using vertices and edges"
     paper = np.ones((500, 500, 3)).astype(np.uint8) * 255
 
     for edge in edges:
@@ -33,11 +44,12 @@ def draw_creature(vertices, edges):
 
 
 def main():
+    "Main function of the module"
+    n = 5
     for _ in range(10):
-        edges = create_species(6)
-        for _ in range(10):
-            vertices = create_vertices(6)
-            draw_creature(vertices, edges)
+        vertices = create_vertices(n)
+        edges = create_edges(n)
+        draw_creature(vertices, edges)
 
 
 if __name__ == "__main__":
