1d312b893a89f58cc666e2cf4dfb8f8cf45b2631
Biraj Rajbhandari <birajr0@gmail.com>
2019-07-23 16:18:56 +0545

    Adds new shapes to environment

--- environment.py
+++ environment.py
@@ -16,16 +16,53 @@ class BodyTypes(Framework):
             shapes=b2EdgeShape(vertices=[(-50, 0), (50, 0)])
         )
 
-        # The platform
-        fixture = b2FixtureDef(
-            shape=b2PolygonShape(box=(4, 0.5)),
+        # # The platform
+        # float dx = x1 - x0; //delta x
+        # float dy = y1 - y0; //delta y
+        # float linelength = sqrtf(dx * dx + dy * dy)
+        # dx /= linelength
+        # dy /= linelength
+        # //Ok, (dx, dy) is now a unit vector pointing in the direction of the line
+        # //A perpendicular vector is given by (-dy, dx)
+        # const float thickness = 5; //Some number
+        # const float px = 0.5 * thickness * (-dy) #perpendicular vector with lenght thickness * 0.5
+        # const float py = 0.5 * thickness * dx
+        # glBegin(GL_QUADS)
+        # glVertex2f(x0 + px, y0 + py)
+        # glVertex2f(x1 + px, y1 + py)
+        # glVertex2f(x1 - px, y1 - py)
+        # glVertex2f(x0 - px, y0 - py)
+        # glEnd()
+
+
+        fixture_1 = b2FixtureDef(
+            shape=b2PolygonShape(vertices=[(5, 5), (10, 10), (1, 10)]),
             density=2,
             friction=0.6,
         )
 
-        self.platform = self.world.CreateDynamicBody(
-            position=(0, 5),
-            fixtures=fixture,
+        fixture_2 = b2FixtureDef(
+            shape=b2PolygonShape(vertices=[(10,10), (15, 5), (15,0)]),
+            density=2,
+            friction=0.6,
+        )
+
+        body_a = self.world.CreateDynamicBody(
+            # shapes=b2EdgeShape(vertices=[(-50, 0), (50, 0)]),
+            # position=(0, 5),
+            fixtures=fixture_1,
+        )
+
+        body_b = self.world.CreateDynamicBody(
+            fixtures=fixture_2,
+            # shapes=b2EdgeShape(vertices=[(10, 10), (15, 5)]),
+            # position=(4, 5),
+        )
+
+        self.world.CreateRevoluteJoint(
+            bodyA=body_a,
+            bodyB=body_b,
+            anchor=(10, 10)
         )
 
 
