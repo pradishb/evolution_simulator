f3ee722af65b4f99b0eb073f1059f623dd8db2c8
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2019-07-25 09:05:29 +0545

    Adds thickness to creature edges

--- environment.py
+++ environment.py
@@ -1,6 +1,9 @@
-from framework.framework import (Framework, Keys, main)
-from Box2D import (b2EdgeShape, b2FixtureDef, b2PolygonShape, b2_dynamicBody,
-                   b2_kinematicBody, b2_staticBody)
+from framework.framework import (Framework, main)
+from Box2D import (b2EdgeShape, b2FixtureDef, b2PolygonShape)
+from maths.maths import line_to_rectangle
+
+
+THICKNESS = 0.5
 
 
 class BodyTypes(Framework):
@@ -16,54 +19,44 @@ class BodyTypes(Framework):
             shapes=b2EdgeShape(vertices=[(-50, 0), (50, 0)])
         )
 
-        # # The platform
-        # float dx = x1 - x0; //delta x
-        # float dy = y1 - y0; //delta y
-        # float linelength = sqrtf(dx * dx + dy * dy)
-        # dx /= linelength
-        # dy /= linelength
-        # //Ok, (dx, dy) is now a unit vector pointing in the direction of the line
-        # //A perpendicular vector is given by (-dy, dx)
-        # const float thickness = 5; //Some number
-        # const float px = 0.5 * thickness * (-dy) #perpendicular vector with lenght thickness * 0.5
-        # const float py = 0.5 * thickness * dx
-        # glBegin(GL_QUADS)
-        # glVertex2f(x0 + px, y0 + py)
-        # glVertex2f(x1 + px, y1 + py)
-        # glVertex2f(x1 - px, y1 - py)
-        # glVertex2f(x0 - px, y0 - py)
-        # glEnd()
+        vertices = [
+            [(5, 5), (10, 10)],
+            [(10, 10), (15, 5)],
+            [(10, 10), (5, 5)],
+        ]
 
+        body = []
 
-        fixture_1 = b2FixtureDef(
-            shape=b2PolygonShape(vertices=[(5, 5), (10, 10), (1, 10)]),
-            density=2,
-            friction=0.6,
-        )
+        for vertex in vertices:
+            point = line_to_rectangle(*vertex, THICKNESS)
 
-        fixture_2 = b2FixtureDef(
-            shape=b2PolygonShape(vertices=[(10,10), (15, 5), (15,0)]),
-            density=2,
-            friction=0.6,
-        )
+            fixture = b2FixtureDef(
+                shape=b2PolygonShape(vertices=point),
+                density=2,
+                friction=0.6,
+            )
 
-        body_a = self.world.CreateDynamicBody(
-            # shapes=b2EdgeShape(vertices=[(-50, 0), (50, 0)]),
-            # position=(0, 5),
-            fixtures=fixture_1,
-        )
+            body.append(
+                self.world.CreateDynamicBody(
+                    fixtures=fixture,
+                )
+            )
 
-        body_b = self.world.CreateDynamicBody(
-            fixtures=fixture_2,
-            # shapes=b2EdgeShape(vertices=[(10, 10), (15, 5)]),
-            # position=(4, 5),
-        )
+        # self.world.CreateRevoluteJoint(
+        #     bodyA=body[0],
+        #     bodyB=body[1],
+        #     anchor=(10, 10),
+        #     collideConnected=False,
+        #     motorSpeed=300,
+        #     maxMotorTorque=300,
+        #     enableMotor=True,
+        # )
 
-        self.world.CreateRevoluteJoint(
-            bodyA=body_a,
-            bodyB=body_b,
-            anchor=(10, 10)
-        )
+        # self.world.CreateRevoluteJoint(
+        #     bodyA=body[0],
+        #     bodyB=body[2],
+        #     anchor=(5, 5)
+        # )
 
 
 if __name__ == "__main__":
