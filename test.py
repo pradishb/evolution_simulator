02a73b4f5d75176a6607fd769135720cb9aa5776
Srijan Tamrakar <tamrakarsrijan98@gmail.com>
2019-11-01 19:10:29 +0545

    added test.py for testing the environment

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-# fot testing purposes
+"Main module"
+import os
+from environment.environment import Environment
+from framework.framework import main
+
+
+def create_directories():
+    "Creates necesesary directories"
+    if not os.path.exists("data"):
+        os.makedirs("data")
+    if not os.path.exists("data/vertices"):
+        os.makedirs("data/vertices")
+    if not os.path.exists("data/edges"):
+        os.makedirs("data/edges")
+
+
+if __name__ == "__main__":
+    create_directories()
+    main(Environment)
