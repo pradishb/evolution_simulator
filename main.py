d29d36c567538b89a3125ec994a2278194f9a921
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2019-11-04 11:31:35 +0545

    Adds load button functionality

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import threading
 import os
 import random
 from copy import copy
+from glob import glob
 
 from PIL import Image, ImageTk
 import easygui
@@ -13,7 +14,7 @@ from environment import Environment
 from framework.framework import main as framework
 from reproduction import reproduce
 from creature import Creature
-from file import save_creature, save_generations
+from file import save_generations, load_generations
 from settings import (
     POPULATION_SIZE, SELECTION_SIZE, OFFSPRINGS_PER_SELECTION_SIZE, RANDOM_NEW_POPULATION_SIZE,
     MIN_VERTICES_COUNT, MAX_VERTICES_COUNT, K_COUNT)
@@ -23,7 +24,6 @@ COL_COUNT = 8
 
 def create_directories():
     "Creates necesesary directories"
-    os.makedirs("data/creatures", exist_ok=True)
     os.makedirs("data/vertices", exist_ok=True)
 
 
@@ -45,9 +45,9 @@ class Application(Gui):
         self.builder.get_object('sort')['state'] = 'disabled'
         self.builder.get_object('do_selection')['state'] = 'disabled'
         self.builder.get_object('reproduce')['state'] = 'disabled'
-        self.builder.get_object('save')['state'] = 'disabled'
 
         self.creatures = []
+        self.serializable_creatures = {}
         self.generations = []
 
         self.scroll_frame = ScrollFrame(self.builder.get_object('creatures_frame'))
@@ -61,10 +61,11 @@ class Application(Gui):
         ''' Creates a generation file '''
         creatures = []
         for creature in self.creatures:
-            save_creature(creature)
+            data = creature.get_data()
+            self.serializable_creatures[creature.identity] = data
             creatures.append(creature.identity)
         self.generations.append(creatures)
-        save_generations(self.generations, 'default')
+        save_generations(self.generations, self.serializable_creatures, 'default')
 
     def create_creature(self, creature, i):
         ''' Creates a single creature '''
@@ -75,8 +76,7 @@ class Application(Gui):
         creature.frame.grid(row=i//COL_COUNT, column=i % COL_COUNT)
         creature.set_description()
         right_click = ContextMenu(self.master, [
-            {'label': 'Test fitness', 'command': lambda c=creature: test_fitness(c)},
-            {'label': 'Test reproduce', 'command': lambda c=creature: test_fitness(c)}])
+            {'label': 'Test fitness', 'command': lambda c=creature: test_fitness(c)}, ])
         creature.description.grid(sticky='w')
         panel = tk.Label(creature.frame)
         panel.bind('<Button-3>', right_click.popup)
@@ -108,6 +108,10 @@ class Application(Gui):
         ''' Do generation button callback '''
         threading.Thread(target=self.threaded_train, daemon=True).start()
 
+    def load(self):
+        ''' Load button callback '''
+        threading.Thread(target=self.threaded_load, daemon=True).start()
+
     def threaded_create(self):
         ''' Creates an initial population of creatures '''
         self.builder.get_object('progress')['value'] = 0
@@ -120,13 +124,11 @@ class Application(Gui):
             progress = i * 100 // POPULATION_SIZE
             self.builder.get_object('progress')['value'] = progress
         self.builder.get_object('progress')['value'] = 0
-        self.builder.get_object('train')['state'] = 'active'
         self.builder.get_object('find_fitness')['state'] = 'active'
 
     def threaded_find_fitness(self, render=True):
         ''' Finds the fitness of all the creatures with render off '''
         self.builder.get_object('progress')['value'] = 0
-        self.builder.get_object('train')['state'] = 'disabled'
         self.builder.get_object('find_fitness')['state'] = 'disabled'
         fitness_es = framework(Environment, render, self.creatures)
         for i, creature in enumerate(self.creatures):
@@ -154,14 +156,14 @@ class Application(Gui):
             self.builder.get_object('progress')['value'] = progress
         self.create_generation()
         self.builder.get_object('progress')['value'] = 0
-        self.builder.get_object('save')['state'] = 'active'
+        self.builder.get_object('train')['state'] = 'active'
         self.builder.get_object('do_selection')['state'] = 'active'
 
     def threaded_selection(self):
         ''' Selects the creatures based on the fitness values '''
         self.builder.get_object('progress')['value'] = 0
-        self.builder.get_object('save')['state'] = 'disabled'
         self.builder.get_object('do_selection')['state'] = 'disabled'
+        self.builder.get_object('train')['state'] = 'disabled'
 
         selected_population = []
         creatures = copy(self.creatures)
@@ -213,20 +215,38 @@ class Application(Gui):
     def threaded_train(self):
         ''' Does training for x generations '''
         try:
+            self.builder.get_object('train')['state'] = 'disabled'
+            self.builder.get_object('find_fitness')['state'] = 'disabled'
             repeat = int(self.builder.get_object('repeat').get())
-            for _ in range(repeat-1):
-                self.builder.get_object('train')['state'] = 'disabled'
-                self.builder.get_object('find_fitness')['state'] = 'disabled'
-                self.threaded_find_fitness(render=False)
-                self.threaded_sort()
+            for _ in range(repeat):
                 self.threaded_selection()
                 self.threaded_reproduce()
-
-            self.threaded_find_fitness(render=False)
-            self.threaded_sort()
+                self.threaded_find_fitness(render=False)
+                self.threaded_sort()
             self.builder.get_object('do_selection')['state'] = 'active'
         except ValueError:
             easygui.msgbox('Make sure the value of X is an integer', 'Error')
+
+    def threaded_load(self):
+        ''' Loads the saved data from file '''
+        file_path = easygui.fileopenbox('Load a generations file', default='./data/generations/')
+        if file_path is None:
+            return
+        data = load_generations(file_path)
+        self.serializable_creatures = data['creatures']
+        self.generations = data['generations']
+        self.builder.get_object('details')['text'] = f'Generation #{len(self.generations)+1}'
+        self.creatures = []
+        for i, creature_id in enumerate(self.generations[-1]):
+            creature_data = self.serializable_creatures[creature_id]
+            creature = Creature(**creature_data, view_port=self.scroll_frame.view_port)
+            self.create_creature(creature, i)
+        self.builder.get_object('create')['state'] = 'disabled'
+        self.builder.get_object('train')['state'] = 'disabled'
+        self.builder.get_object('find_fitness')['state'] = 'disabled'
+        self.builder.get_object('sort')['state'] = 'disabled'
+        self.builder.get_object('do_selection')['state'] = 'active'
+        self.builder.get_object('reproduce')['state'] = 'disabled'
 
 
 def main():
